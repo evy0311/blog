93b53625bcdc7d8517dd0f68d306058ed74ceadb
Evan <evy0311@gmail.com>
2020-01-22 15:06:00 -0500

    Updating config

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/2018/07/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/blog/2018/07/01/welcome.html"}]};
