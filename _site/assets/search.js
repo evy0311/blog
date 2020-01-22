54272086276293edaed96275f0baf5f9565000e1
Evan <evy0311@gmail.com>
2020-01-22 15:03:03 -0500

    Deplying default theme site to see if it works

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome to my blog!",'url':"/blog/new/2020/01/22/welcome-to-jekyll.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/2018/07/01/welcome.html"}]};
