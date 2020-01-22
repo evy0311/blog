05e91094c2ad26a9e60bd028d8f3a0069d4bd409
Evan <evy0311@gmail.com>
2020-01-22 13:03:39 -0500

    Okay were almost there

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/blog/2018/07/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/blog/2018/07/01/welcome.html"},{'title':"Welcome",'url':"/blog/blog/2018/07/01/welcome.html"}]};
